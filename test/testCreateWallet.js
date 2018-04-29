cceb2bb361078d29d79a5a97a3e05815d2dcc44d
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2018-04-30 01:01:10 +0200

    Changed outdated use of lodash _.contains to _.includes

--- test/testCreateWallet.js
+++ test/testCreateWallet.js
@@ -1,6 +1,6 @@
 //put in new directory, run 'npm i bitcore-wallet-client-colx' and then execute with 'node'
 var Client = require('bitcore-wallet-client-colx');
-var BWS_INSTANCE_URL = 'http://localhost:3232/bws/api'
+var BWS_INSTANCE_URL = 'http://84.200.210.11:3232/bws/api'
 var client = new Client({ baseUrl: BWS_INSTANCE_URL, verbose: false });
 client.createWallet("MyWallet", "Me", 2, 2, {network: 'livenet'}, function(err, secret) {
   if (err) {
